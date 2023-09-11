4c2018a86d245098c242fb7bbd04bf383259b623
xharris <xhh950@gmail.com>
2023-09-10 23:17:53 -0400

    ⚰️ Removes unused service worker

--- nate.tv/service-worker.js
+++
@@ -1,9 +0,0 @@
-const INITIAL_STORAGE = {
-    vods: {} // { url:  }
-}
-
-chrome.runtime.onInstalled.addListener(({ reason }) => {
-    if (reason === 'install') {
-        chrome.storage.local.set(INITIAL_STORAGE)
-    }
-})
